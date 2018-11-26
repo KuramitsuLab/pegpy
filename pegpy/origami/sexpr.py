688da95bf72fb38dc270afd686d0f415eba40b15
KASE <kaseyutaka.suffix@gmail.com>
2018-11-26 19:03:35 +0900

    modified origami

--- pegpy/origami/sexpr.py
+++ pegpy/origami/sexpr.py
@@ -71,6 +71,7 @@ class SExpr(object):
         'CharExpr': lambda t: Char(t.asString()),
         'TrueExpr': lambda t: 'true',
         'FalseExpr': lambda t: 'false',
+        'NullExpr': lambda t: 'null',
     }
 
     @classmethod
